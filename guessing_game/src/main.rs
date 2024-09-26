8a49dfdf26355785771a73faeca16c4b3341a5fa
wjp <2435878928@qq.com>
2024-09-26 17:04:10 +0800

    fix: add comment

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,28 @@
 use std::{cmp::Ordering, io};
 
 use rand::Rng;
+
 fn main() {
+    // 打印欢迎信息
     println!("Guess the number!");
     println!("Please input your guess.");
 
+    // 生成一个1到100之间的随机数
     let random_num = rand::thread_rng().gen_range(1..101);
 
+    // 无限循环，直到用户猜对数字或输入退出命令
     loop {
+        // 读取用户输入的数字，如果用户输入"quit"，则退出循环
         let Some(guess) = read_number() else {
             break;
         };
+
+        // 比较用户输入的数字和随机数
         match random_num.cmp(&guess) {
-            Ordering::Less => println!("Too big!"),
-            Ordering::Greater => println!("Too small!"),
+            Ordering::Less => println!("Too big!"), // 用户输入的数字太大
+            Ordering::Greater => println!("Too small!"), // 用户输入的数字太小
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win!"); // 用户猜对了数字
                 break;
             }
         }
@@ -25,13 +32,17 @@ fn main() {
 fn read_number() -> Option<u32> {
     loop {
         let mut guess = String::new();
+        // 读取用户输入的一行
         if let Ok(_) = io::stdin().read_line(&mut guess) {
+            // 尝试将输入解析为数字
             if let Ok(num) = guess.trim().parse::<u32>() {
                 return Some(num);
             } else {
+                // 如果用户输入"quit"，则返回None
                 if "quit".eq(&guess.trim().to_lowercase()) {
                     return None;
                 } else {
+                    // 提示用户输入一个数字
                     println!("Please input a number!");
                 }
             }
