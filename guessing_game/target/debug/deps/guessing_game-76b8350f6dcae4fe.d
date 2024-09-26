a02d179d0a649d0d49d8757548ea75d6de4749ea
wjp <2435878928@qq.com>
2024-09-26 15:27:04 +0800

    feature: init guess game and add git ignore file

---
+++ guessing_game/target/debug/deps/guessing_game-76b8350f6dcae4fe.d
@@ -0,0 +1,5 @@
+D:\rust-basic\guessing_game\target\debug\deps\libguessing_game-76b8350f6dcae4fe.rmeta: src/main.rs
+
+D:\rust-basic\guessing_game\target\debug\deps\guessing_game-76b8350f6dcae4fe.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+D:\rust-basic\guessing_game\target\debug\guessing_game.exe: D:\rust-basic\guessing_game\src\main.rs
